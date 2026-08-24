6e77bfcac21ea5ab796e05b61057b9c5b15fdb3a
voidcons0le <thaycos@gmail.com>
2026-08-24 11:44:09 -0400

    oop

--- js/layers.js
+++ js/layers.js
@@ -187,7 +187,7 @@ addLayer("s", {
     color: "khaki",
     requires: new Decimal(8), // Can be a function that takes requirement increases into account
     resource: "stars", // Name of prestige currency
-    baseResource: "matter", // Name of resource prestige is based on
+    baseResource: "planets", // Name of resource prestige is based on
     baseAmount() {return player.p.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
